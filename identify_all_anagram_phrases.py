5a1f6d73df7055b68329e8af83bbe3018e380139
Sadie Levin <sadiew.levin@gmail.com>
2024-07-19 13:06:03 -0700

    efficiency modification- instead of searching every word, program now only searches among possible words. This does not decrease the time complexity, but it does speed the program up substantially.

--- identify_all_anagram_phrases.py
+++ identify_all_anagram_phrases.py
@@ -3,7 +3,8 @@ from english import ENGLISH_WORDS
 #accepts only lowercase letters and spaces
 def run(input):
     input_dict = deconstruct_string(input)
-    identify_all_substrings(input_dict)
+    all_possible_substrings = identify_possible_substrings(input_dict)
+    identify_all_anagrams(input_dict, found_word_list = [], possible_word_list = all_possible_substrings)
     print("All done!")
 
 #takes all characters in string, turns them into 
@@ -17,7 +18,26 @@ def deconstruct_string(input_string):
                 output_dict.update({character:1})
     return output_dict
 
-def identify_all_substrings(input_dict, found_word_list = []):
+def identify_possible_substrings(input_dict):
+    all_possible_substrings = []
+    for test_word in ENGLISH_WORDS:
+        working_input_dict_for_all_possible_substrings = input_dict.copy()
+        test_dict = deconstruct_string(test_word)
+        is_valid_substring = True
+        for test_letter in test_dict:
+            try:
+                if test_dict[test_letter] > input_dict[test_letter]:
+                    is_valid_substring = False
+                else:
+                    working_input_dict_for_all_possible_substrings[test_letter] -= 1
+            except:
+                is_valid_substring = False
+                break
+        if is_valid_substring:
+            all_possible_substrings.append(test_word)
+    return all_possible_substrings
+
+def identify_all_anagrams(input_dict, found_word_list = [], possible_word_list = ENGLISH_WORDS):
     #checks if base case found, returns None if input dict has no remaining letters
     finished = True
     for element in input_dict:
@@ -33,7 +53,7 @@ def identify_all_substrings(input_dict, found_word_list = []):
         return
     
     #checks every english word to see if it could be constructed using input dict's characters
-    for test_word in ENGLISH_WORDS:
+    for test_word in possible_word_list:
         working_input_dict = input_dict.copy()
         test_dict = deconstruct_string(test_word)
         is_valid_substring = True
@@ -52,7 +72,8 @@ def identify_all_substrings(input_dict, found_word_list = []):
         if is_valid_substring:
             temp_found_word_list = found_word_list.copy()
             temp_found_word_list.append(test_word)
-            identify_all_substrings(working_input_dict, temp_found_word_list)
+            identify_all_anagrams(working_input_dict, temp_found_word_list, possible_word_list)
+
 
 if __name__ == "__main__":
     run(input("Enter the word or phrase you would like all anagrams of!\n>>> ").lower())
